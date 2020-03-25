a6d120709ad2199c96f5676d9aa6ead8aeea15f7
j21166675 <johney.mkc@gmail.com>
2020-03-25 13:35:08 -0700

    SpringBoot-RestApi

--- RestApiMovies/src/main/java/com/springboot/movieflix/main/ManageMoviesList.java
+++ RestApiMovies/src/main/java/com/springboot/movieflix/main/ManageMoviesList.java
@@ -2,6 +2,8 @@ package com.springboot.movieflix.main;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 import java.util.List;
 
 import java.util.ArrayList;
@@ -9,12 +11,13 @@ import java.util.ArrayList;
 @RestController
 public class ManageMoviesList {
     @RequestMapping("/movielist")
-    public List<MovieList> retMovieList(){
-        ArrayList<MovieList> list = new ArrayList<>();
-        list.add(new MovieList("Good News", "Hindi", "Comedy"));
-        list.add(new MovieList("Invisible Guest", "Tamil", "Detective"));
-        list.add(new MovieList("Fantastic 5", "English", "scifi"));
-        list.add(new MovieList("Master", "Tamil", "Comedy"));
-        return list;
+    public List<MovieList> retMovieList() throws Exception{
+
+        return Arrays.asList(
+                new MovieList("Good News", "Hindi", "Comedy"),
+                new MovieList("Invisible Guest", "Tamil", "Detective"),
+                new MovieList("Fantastic 5", "English", "scifi"),
+                new MovieList("Master", "Tamil", "Comedy"));
+
     }
 }
